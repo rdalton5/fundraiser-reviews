506f3ad950ae967c34982f0fd7b859d583653947
Robert Dalton <rdalton5@kent.edu>
2021-10-13 22:52:32 -0400

    Clean up fundraiser view and add navigation link

--- resources/views/fundraisers/all.blade.php
+++ resources/views/fundraisers/all.blade.php
@@ -1,8 +1,3 @@
-{{-- @extends('layouts.app')
-@section('content')
-    
-@stop --}}
-
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -10,27 +5,11 @@
         </h2>
     </x-slot>
 
-    {{-- <div class="py-12"> --}}
-    <div id="app">
-        <example-component></example-component>
-        <h2>Fundraisers</h2>
-        <ul>
-            @foreach($fundraisers as $fundraiser)
-                <fundraiser-card v-bind:fundraiser="{{json_encode($fundraiser)}}"></fundraiser-card>
-                {{-- <li>
-                    <p>{{$fundraiser->id}} {{$fundraiser->name}}</p>
-                    <ul>
-                        @foreach($fundraiser->reviews as $review)
-                            <li>{{$review->rating}} {{$review->review}}</li>
-                        @endforeach
-                    </ul>
-                </li> --}}
-            @endforeach
-        </ul>
-        
+    <div id="app" class="m-5">
+        @foreach($fundraisers as $fundraiser)
+            <fundraiser-card class="m-1" v-bind:fundraiser="{{json_encode($fundraiser)}}"></fundraiser-card>
+        @endforeach
     </div>
-        
-        
-    {{-- </div> --}}
+    
     <script src="{{ mix('/js/app.js') }}"></script>
 </x-app-layout>
